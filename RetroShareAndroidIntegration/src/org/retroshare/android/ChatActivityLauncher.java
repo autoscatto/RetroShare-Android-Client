1860ca83f376864611d0853944dabf07969fd562
autoscatto <4utoscatto@gmail.com>
2013-06-21 15:07:49 +0200

    Pulizia merge

--- RetroShareAndroidIntegration/src/org/retroshare/android/ChatActivityLauncher.java
+++ RetroShareAndroidIntegration/src/org/retroshare/android/ChatActivityLauncher.java
@@ -13,35 +13,20 @@ import rsctrl.chat.Chat;
  * XXX: da rifare completamente, e' solo un test
  * ah, trovare un intent migliore rispetto a android.intent.action.VIEW (esiste?)
  */
-<<<<<<< HEAD
-public class ChatActivityLauncher extends ProxiedActivityBase {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-=======
 public class ChatActivityLauncher extends Activity
 {
-	public String TAG() { return "ChatActivityLauncher"; }
->>>>>>> upstream/stickyproxy
+    public String TAG() { return "ChatActivityLauncher"; }
 
-	@Override
+    @Override
     public void onCreate(Bundle savedInstanceState)
     {
-		super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
 
-		Log.d(TAG(), "onCreate(Bundle savedInstanceState)");
+        Log.d(TAG(), "onCreate(Bundle savedInstanceState)");
 
         if (getIntent().getData() != null)
         {
             Cursor cursor = managedQuery(getIntent().getData(), null, null, null, null);
-<<<<<<< HEAD
-            if (cursor.moveToNext()) {
-                String sslid = cursor.getString(cursor.getColumnIndex("DATA2"));
-                Intent i=new Intent(ChatActivityLauncher.this,ChatActivity.class);
-                i.putExtra("ChatId", Chat.ChatId.newBuilder().setChatType(Chat.ChatType.TYPE_PRIVATE).setChatId(sslid).build().toByteArray());
-                // keine lobby info
-                //i.putExtra("ChatLobbyInfo", lobbyInfo.toByteArray());
-                startActivity(ChatActivity.class, i);
-=======
             if (cursor.moveToNext())
             {
                 String serverName = cursor.getString(cursor.getColumnIndex(ContactsContract.RawContacts.ACCOUNT_NAME));
@@ -49,9 +34,8 @@ public class ChatActivityLauncher extends Activity
 
                 Intent i = new Intent(this, ChatActivity.class);
                 i.putExtra(ChatActivity.CHAT_ID_EXTRA, Chat.ChatId.newBuilder().setChatType(Chat.ChatType.TYPE_PRIVATE).setChatId(sslid).build().toByteArray());
-				i.putExtra(ChatActivity.SERVER_NAME_EXTRA, serverName);
+                i.putExtra(ChatActivity.SERVER_NAME_EXTRA, serverName);
                 startActivity(i);
->>>>>>> upstream/stickyproxy
                 //XXX: mi suicido subito dopo aver lanciato l'altra activity
                 // tranquillo perche' ho il nohistory sul manifest (http://developer.android.com/guide/topics/manifest/activity-element.html#nohist)
                 finish();
